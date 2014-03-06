31840ac093434ce6130ab15c2cccc2ab5a9f6dac
Trond Hindenes <trond@hindenes.com>
2014-03-06 08:50:09 +0100

    Initial Commit

---
+++ AmazonAwsExtensions/Route53-CreateRecord.ps1
@@ -0,0 +1,114 @@
+﻿#PowerShell implementation based on c# example found here:
+#https://gist.github.com/j3tm0t0/2024833
+
+Function Create-AwsRoute53Record
+{
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    Param (
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [Amazon.Runtime.BasicAWSCredentials]$credentials,
+        
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [Amazon.Route53.Model.HostedZone]$Zone,
+        
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [Amazon.RegionEndpoint]$region = [Amazon.RegionEndpoint]::USWest1,
+
+        [ValidateSet("A", "SOA", "PTR", "MX", "CNAME","TXT","SRV","SPF","AAAA","NS")]
+        [String]$RecordType,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Name,
+
+        [Parameter(Mandatory=$true)]
+        $value,
+
+        [int]$TTL,
+        
+        # If wait is specified, the function will wait until AWS confirms that the change is in sync, 
+        #meaning it has been written to the zone
+        $wait = $false,
+
+        # If wait is specified, waitinterval specifies the interval in miliseconds between each polling
+        [int]$waitinterval = 1000
+
+
+        )
+
+    Begin 
+    {
+        $route53client = New-Object Amazon.Route53.AmazonRoute53Client -ArgumentList $creds,$region
+    }
+
+    Process
+    {
+        $records = @()
+        foreach ($valueentry in $value)
+            {
+                $Record = new-object Amazon.Route53.Model.ResourceRecord
+                $record.Value = $valueentry
+                $records += $Record
+                $record = $null
+
+            }
+
+        #add the trailing dot
+        if (!($Name.EndsWith(".")) -and $Name)
+            {$Name += "."}
+
+        $ResourceRecordSet = New-Object Amazon.Route53.Model.ResourceRecordSet
+        $ResourceRecordSet.Type = $RecordType
+        $ResourceRecordSet.ResourceRecords = $Records
+        $ResourceRecordSet.Name = $Name
+        $ResourceRecordSet.TTL = $ttl
+
+        $changebatch = New-Object Amazon.Route53.Model.ChangeBatch
+        $change = New-Object Amazon.Route53.Model.Change
+        $change.Action = "Create"
+        $change.ResourceRecordSet = $ResourceRecordSet
+        $changebatch.Changes = $change
+
+        $ChangeResourceRecordSetsRequest = new-object Amazon.Route53.Model.ChangeResourceRecordSetsRequest
+        $ChangeResourceRecordSetsRequest.HostedZoneId = $Zone.Id
+        $ChangeResourceRecordSetsRequest.ChangeBatch = $changebatch
+
+        Try
+        {
+            $result = $route53client.ChangeResourceRecordSets($ChangeResourceRecordSetsRequest)
+        }
+        Catch [system.Exception]
+        {
+            Write-error $error[0]
+        }
+        
+        if ($result)
+        {
+            $result = $result.ChangeInfo
+            if ($wait)
+            {
+            #Keep polling the changeinfo until it is no longer pending
+            Do 
+                {
+                    #get change status
+                    if ($SecondPoll)
+                        {Start-Sleep -Milliseconds $waitinterval}
+                    $changeGet = new-object Amazon.Route53.Model.GetChangeRequest
+                    $changeGet.Id = $result.Id
+                    $status = $route53client.GetChange($changeGet)
+                    $SecondPoll = $true
+                    Write-verbose "Waiting for changes to sync. Current status is $($status.ChangeInfo.Status.Value)"
+                }
+            Until ($status.ChangeInfo.Status.Value -eq "INSYNC")
+
+        
+            }
+        
+            $Status
+
+        }
+
+        
+    }
+}
+
+
---
+++ AmazonAwsExtensions/Route53-GetFunctions.ps1
@@ -0,0 +1,86 @@
+﻿#$creds = Get-AWSCredentials -StoredCredentials "trond@hindenes.com"
+
+Function Get-AwsRoute53Zone 
+{
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    Param (
+        [Parameter(Position=0)]
+        [string]$ZoneName,
+        
+        # The credentials object should be generated from running Get-AWSCredentials
+        [Parameter(Mandatory=$true)]
+        [Amazon.Runtime.BasicAWSCredentials]$credentials,
+
+        [Amazon.RegionEndpoint]$region = [Amazon.RegionEndpoint]::USWest1,
+        $AddCredstoReturnedObject = $true,
+        $AddRegiontoReturnedObject = $true
+
+    )
+
+    #add the trailing dot
+    if (!($ZoneName.EndsWith(".")) -and $ZoneName)
+        {$ZoneName += "."}
+
+    $route53client = New-Object Amazon.Route53.AmazonRoute53Client -ArgumentList $creds,$region
+
+    $hostedZones = $route53client.ListHostedZones()
+    $zone = $hostedZones.HostedZones
+
+    if ($ZoneName)
+        {
+            $zone = $zone | where {$_.Name -like $ZoneName}
+            return $zone
+        }
+    if ($AddCredstoReturnedObject)
+    {
+        $zone | ForEach-Object {
+            $zone | Add-Member -MemberType NoteProperty -Name "Credentials" -Value $credentials -Force
+            }
+    }
+
+    if ($addregionToReturnedObject)
+    {
+        $zone | ForEach-Object {
+            $zone | Add-Member -MemberType NoteProperty -Name "Region" -Value $region -Force
+            }
+    }
+
+    $zone
+   
+}
+
+Function Get-AwsRoute53Record
+{
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    Param (
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [Amazon.Runtime.BasicAWSCredentials]$credentials,
+        
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [Amazon.Route53.Model.HostedZone]$Zone,
+        
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [Amazon.RegionEndpoint]$region = [Amazon.RegionEndpoint]::USWest1,
+
+        [ValidateSet("A", "SOA", "PTR", "MX", "CNAME","TXT","SRV","SPF","AAAA","NS")]
+        $RecordType
+    )
+
+    Begin {}
+    Process
+    {
+        $route53client = New-Object Amazon.Route53.AmazonRoute53Client -ArgumentList $creds,$region
+        $ZonesRequest = New-Object -TypeName Amazon.Route53.Model.ListResourceRecordSetsRequest
+        Write-verbose "Retrieving records for zone $($zone.id) using region $region"
+        $ZonesRequest.HostedzoneId = $zone.Id
+        $Recordset = $route53client.ListResourceRecordSets($ZonesRequest)
+        $records = $Recordset.ResourceRecordSets
+        if ($RecordType)
+            {
+                $records  = $records | where {$_.Type -eq $RecordType}
+            }
+        
+        $Records
+
+    }
+}
