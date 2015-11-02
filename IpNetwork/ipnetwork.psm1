bc228117728aa79e58f598445a4ed0aa2804bcd2
trondhindenes <trond@hindenes.com>
2015-11-02 14:29:10 +0100

    Added new functions

--- IpNetwork/ipnetwork.psm1
+++ IpNetwork/ipnetwork.psm1
@@ -15,3 +15,41 @@ Function Get-IpNetworkAddresses
     [LukeSkywalker.IPNetwork.IPAddressCollection]$addresscollection = [LukeSkywalker.IPNetwork.IPNetwork]::ListIPAddress($IPNetwork)
     $addresscollection
 }
+
+Function Get-IPNetworkFromIpAddress
+{
+    Param (
+        [Parameter(Mandatory=$true,ParameterSetName='CIDR')]
+        [string]$IpAddressWithCidr,
+        
+        [Parameter(Mandatory=$true,ParameterSetName='IPandsubnet')]
+        [string]$IpAddress,
+        [Parameter(Mandatory=$true,ParameterSetName='IPandsubnet')]
+        [string]$Subnetmask
+    )
+
+    if ($IpAddress)
+    {
+        $cidr = Get-SubnetCidr -Subnet $Subnetmask
+        $IpAddressWithCidr = "$ipaddress/$cidr"
+    }
+
+    if ($IpAddressWithCidr.Split("/").count -ne 2)
+    {
+        Write-error "Couldn't parse address. Specify as '192.168.1.5/24'"
+        break
+    }
+    [LukeSkywalker.IPNetwork.IPNetwork]::Parse($IpAddressWithCidr)
+}
+
+Function Get-SubnetCidr
+{
+    Param ([String]$Subnet)
+    if ($subnet.split(".").count -ne 4)
+    {
+        Write-error "Couldn't parse address. Specify as '255.255.255.0'"
+        break
+    }
+    [LukeSkywalker.IPNetwork.IPNetwork]::ToCidr($subnet)
+}
+
